261a8436df6fce21558d4d24693992785dffe21b
Sachit Nanajkar <sachitnjk@gmail.com>
2022-11-05 16:37:05 +0530

    Class -1
    Variables
    Strings
    Scope
    What is ;
    
    Function
    In Parmas
    Return type
    Overloads
    Casting
    Passing by ref
    
    Class
    Construter
    Destruter
    Meber variables and functions
    namespaces
    structs
    --> Move all from program to a class

---
+++ ConsoleApp1/BasicsLearnCode.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHashLearn
+{
+	internal class BasicsLearnCode
+	{
+
+		private float xPos, yPos;
+
+		public BasicsLearnCode()
+		{
+			xPos = 0;
+			yPos = 0;
+		}
+
+		public BasicsLearnCode(float xPos, float yPos)
+		{
+			this.xPos = xPos;
+			this.yPos = yPos;
+
+			Console.WriteLine($"xPos:{this.xPos}, yPos{this.yPos}");
+		}
+
+
+		public void ExamplePassWithRef()
+		{
+			float anotherFloat = 40f;
+			Subtract(10, 5, anotherFloat);
+			Console.WriteLine(anotherFloat);                          //withought ref, change to outValue does not affect anotherFloat	
+			SubtractWithRef(10, 5, ref anotherFloat);
+			Console.WriteLine(anotherFloat);                          //with ref, change to outValue affects anotherFloat
+		}
+
+		void Subtract(float a, float b, float outValue)
+		{
+			outValue = a - b;
+
+		}
+
+		void SubtractWithRef(float a, float b, ref float outValue)
+		{
+			outValue = a - b;
+		}
+
+
+  
+
+		public void ExampleOfOverloading()
+		{
+			int addedValue = (int)AddFunction(5, 10);         //AddFunction() here is casted into int - returns float by default
+			float addedValue2 = AddFunction(5, 10, 20);       //AddFunction() here not casted into int - returns float by default
+		}
+
+		float AddFunction(float a, float b)
+		{
+			float c = a + b;
+
+			return c;
+
+		}
+
+		float AddFunction(float a, float b, float d)
+		{
+			float c = a + b + d;
+
+			return c;                                   //return is always the endline(terminatin point)
+
+		}
+
+
+
+		public void ExampleOfCasting()
+		{
+			float testFloat = 10.8f;
+			int floatToInt = (int)testFloat;   // Gives op as 10 as it gives the lower limit
+			Console.WriteLine(floatToInt);
+		}
+
+
+
+		int CanSeePlayer(bool playerInView, bool allyInView)
+		{
+			if (playerInView)
+			{
+				return 1;
+				//int jeebs = 2;							// this will not run as return is the termination point
+			}
+
+			if (allyInView)
+			{
+				return 3;
+			}
+			else
+			{
+				return 4;                                 // there should always be a return value present for it to work if no return value is specified then error
+			}
+		}
+
+
+	}
+
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+namespace CHashLearn // Note: actual namespace depends on the project name.
+{
+	internal class Program
+	{
+
+		static void Main(string[] args)
+		{
+
+
+			BasicsLearnCode newItem = new BasicsLearnCode();
+			newItem.ExamplePassWithRef();
+			newItem.ExampleOfCasting();
+			newItem.ExampleOfOverloading();
+
+
+		}
+
+
+	}
+}
